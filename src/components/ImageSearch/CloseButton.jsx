deaff8cc184242ef7147e671f222c509eef1ef2b
yashgoyal0110 <149111979+yashgoyal0110@users.noreply.github.com>
2024-12-19 20:18:36 +0530

    Delete src directory

--- src/components/ImageSearch/CloseButton.jsx
+++
@@ -1,9 +0,0 @@
-function CloseButton({ onClose }) {
-    return (
-      <button className="close-button" onClick={onClose}>
-        ✕
-      </button>
-    );
-  }
-  
-  export default CloseButton;
